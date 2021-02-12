e42da7adcfc932dceeaab7e89428a4e04aa444f1
Cameron Nasseri <cameron.nasseri@verizon.net>
2021-02-12 16:06:14 -0500

    I made a change to the file

---
+++ resources/js/app.js
@@ -0,0 +1,25 @@
+class HashMap {
+    constructor(size = 0) {
+      this.hashmap = new Array(size)
+        .fill(null);
+    }
+
+  ash(key) {
+    let hashCode = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashCode += hashCode + key.charCodeAt(i);
+    }
+    return hashCode % this.hashmap.length;
+}
+
+assign(key, value) {
+    const arrayIndex = this.hash(key);
+    this.hashmap[arrayIndex] = value;
+  }
+
+etrieve(key) {
+    const arrayIndex = this.hash(key);
+    return this.hashmap[arrayIndex];
+  }
+
+module.exports = HashMap;
